97c3b46aa6416248b7b0c533051397a381492b1e
Tom Willemse <tom@ryuslash.org>
2013-08-07 22:27:50 +0200

    Bump version to 1.1.0

--- desktop-registry.el
+++ desktop-registry.el
@@ -4,7 +4,7 @@
 
 ;; Author: Tom Willemse <tom@ryuslash.org>
 ;; Keywords: convenience
-;; Version: 1.0.0
+;; Version: 1.1.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by

89031aacaceee01fdef373100902c5479d1eee12
Tom Willemse <tom@ryuslash.org>
2014-01-19 20:42:48 +0100

    Add URL header

--- desktop-registry.el
+++ desktop-registry.el
@@ -5,6 +5,7 @@
 ;; Author: Tom Willemse <tom@ryuslash.org>
 ;; Keywords: convenience
 ;; Version: 1.1.0
+;; URL: http://projects.ryuslash.org/desktop-registry/
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
